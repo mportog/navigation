3ec4fb0bd2d5e4622d17fa9aac8923bcbefd2783
Matheus Porto <matheus.garcia@t-systems.com.br>
2021-05-29 21:30:46 -0300

    add navigation graph

---
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/ProdutosActivity.kt
@@ -0,0 +1,15 @@
+package br.com.alura.aluraesporte.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.aluraesporte.R
+
+
+class ProdutosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.produtos_activity)
+    }
+
+}
---
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
@@ -0,0 +1,65 @@
+package br.com.alura.aluraesporte.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.extensions.formatParaMoedaBrasileira
+import br.com.alura.aluraesporte.model.Produto
+import br.com.alura.aluraesporte.ui.activity.CHAVE_PRODUTO_ID
+import br.com.alura.aluraesporte.ui.viewmodel.DetalhesProdutoViewModel
+import kotlinx.android.synthetic.main.detalhes_produto.*
+import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
+
+class DetalhesProdutoFragment : Fragment() {
+
+    private val produtoId by lazy {
+        arguments?.getLong(CHAVE_PRODUTO_ID)
+            ?: throw IllegalArgumentException(ID_PRODUTO_INVALIDO)
+    }
+    private val viewModel: DetalhesProdutoViewModel by viewModel { parametersOf(produtoId) }
+    private val controlador by lazy { findNavController() }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.detalhes_produto,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buscaProduto()
+        configuraBotaoComprar()
+    }
+
+    private fun configuraBotaoComprar() {
+        detalhes_produto_botao_comprar.setOnClickListener {
+            viewModel.produtoEncontrado.value?.let {
+                val dados = Bundle()
+                dados.putLong(CHAVE_PRODUTO_ID, produtoId)
+                controlador.navigate(R.id.pagamento, dados)
+            }
+        }
+    }
+
+    private fun buscaProduto() {
+        viewModel.produtoEncontrado.observe(viewLifecycleOwner, Observer {
+            it?.let { produto ->
+                detalhes_produto_nome.text = produto.nome
+                detalhes_produto_preco.text = produto.preco.formatParaMoedaBrasileira()
+            }
+        })
+    }
+
+}
